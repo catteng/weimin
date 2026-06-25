33561e187fd258eba9cfad57379a0d9cd349093e
weiwei.teng <vivi930038@gmail.com>
2026-06-25 14:18:53 +0800

    init

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style lang="scss">
+@use '../public/assets/css/normalize';
+@use '../public/assets/css/layout';
+@use '../public/assets/css/main';
+</style>
+
