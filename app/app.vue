fb68db9d31830fa873ee20b41a6fee803b47197b
weiwei.teng <vivi930038@gmail.com>
2026-06-30 17:06:20 +0800

    [ Layout ] : og & favicon setting

--- app/app.vue
+++ app/app.vue
@@ -4,7 +4,7 @@ useSeoMeta({
   ogTitle: '蔚泯 Weimin | 熱銷商品限時優惠中 - 您的購物首選商店',
   description: '蔚泯 Weimin 致力於提供高品質的商品與無與倫比的服務。從生活用品到專業器材，我們嚴選每一件商品，只為了給您最棒的購買體驗。立即瀏覽我們的官方網站，探索更多驚喜優惠。',
   ogDescription: '探索 蔚泯 Weimin 的精選商品！我們提供高品質的選擇與完善的售後服務，現在就點擊查看最新優惠，享受便利、安全且愉快的購物時光。',
-  ogImage: 'https://weimin-demo.teng-teng.workers.dev/images/logo/logo-lg.svg', // 修正路徑：不需包含 /public
+  ogImage: 'https://weimin-demo.teng-teng.workers.dev/images/logo/og.png',
   ogSiteName: '蔚泯 Weimin',
   twitterCard: 'summary_large_image',
 })
