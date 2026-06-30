57cc790927bf2572447a02b6f0985cc537809866
weiwei.teng <vivi930038@gmail.com>
2026-06-30 16:49:00 +0800

    [ Layout ] : og setting

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: '蔚泯',
+  ogTitle: '標題標題標題標題標題標題標題',
+  description: 'goole看的描述文字文字文字文字文字文字文字文字文字文字',
+  ogDescription: '社群分享專用描述文字文字文字文字',
+  ogImage: '/app/assets/images/logo/logo-lg.svg',
+  // twitterCard: 'summary_large_image',
+})
+</script>
+
 <template>
   <div>
     <NuxtPage />
