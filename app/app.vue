ab28cc5297982a2aecb094ae6fffab75e747840b
weiwei.teng <vivi930038@gmail.com>
2026-08-18 11:16:25 +0800

    [ sit ] : og image change

--- app/app.vue
+++ app/app.vue
@@ -4,7 +4,7 @@ useSeoMeta({
   ogTitle: '蔚泯WEIMIN｜廣告・KOL短影音・SEO/GEO・視覺策略・品牌整合',
   description: '從視覺的起點到數據的終點，用策略重塑品牌真正的影響力。你負責把產品做到極致，守護你的初心；我們的任務，是當你隨調隨用的最強後盾，用文字賦予靈魂、用數據死磕轉換，幫你把這份執著與美好，轉化為看得到的實質結果。',
   ogDescription: '從視覺的起點到數據的終點，用策略重塑品牌真正的影響力。你負責把產品做到極致，守護你的初心；我們的任務，是當你隨調隨用的最強後盾，用文字賦予靈魂、用數據死磕轉換，幫你把這份執著與美好，轉化為看得到的實質結果。',
-  ogImage: 'https://weimin-demo.teng-teng.workers.dev/images/logo/og.png',
+  ogImage: 'https://weimin-demo.teng-teng.workers.dev/images/logo/meta-og.jpg',
   ogSiteName: '蔚泯 WEIMIN',
 })
 </script>
